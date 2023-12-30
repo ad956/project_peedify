c0aa937ae65ee8d3ee04f194b6eae71e21a2a837
Anand Suthar <anandsuthar956@gmail.com>
2023-12-30 10:36:44 +0530

    form added for client details

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,81 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final _formKey = GlobalKey<FormState>();
+    final TextEditingController nameController = TextEditingController();
+    final TextEditingController addressController = TextEditingController();
+    final TextEditingController billNumberController = TextEditingController();
+    final TextEditingController dateController = TextEditingController();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Peedify"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  controller: nameController,
+                  decoration:
+                      const InputDecoration(labelText: 'M/s (Name of Client)'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter the client name';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: addressController,
+                  decoration: const InputDecoration(labelText: 'Address'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter the address';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: billNumberController,
+                  decoration:
+                      const InputDecoration(labelText: 'No : (Bill Number)'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter the bill number';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: dateController,
+                  decoration: const InputDecoration(labelText: 'Date'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter the date';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Process the form data
+                      // For example, you can print the data to the console
+                      print('Name: ${nameController.text}');
+                      print('Address: ${addressController.text}');
+                      print('Bill Number: ${billNumberController.text}');
+                      print('Date: ${dateController.text}');
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
