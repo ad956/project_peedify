59db508176d4c8a84031ce442cfde2729af80514
Anand Suthar <anandsuthar956@gmail.com>
2023-12-30 11:34:33 +0530

    go_router configured

---
+++ lib/routes/router_config.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:peedify/routes/route_constants.dart';
+import 'package:peedify/screens/bill_screen.dart';
+import 'package:peedify/screens/error_screen.dart';
+import 'package:peedify/screens/home_screen.dart';
+
+class PeedifyNavigator {
+  final GoRouter router = GoRouter(
+      initialLocation: "/",
+      navigatorKey: GlobalKey<NavigatorState>(),
+      errorBuilder: ((context, state) => const ErrorScreen()),
+      routes: [
+        GoRoute(
+          name: RouteNames.homeScreen,
+          path: "/",
+          builder: (context, state) => const HomeScreen(),
+        ),
+        GoRoute(
+            name: RouteNames.billScreen,
+            path: "/bill",
+            pageBuilder: (context, state) {
+              return CustomTransitionPage(
+                  key: state.pageKey,
+                  transitionDuration: const Duration(milliseconds: 1300),
+                  child: BillDetailsForm(),
+                  transitionsBuilder:
+                      ((context, animation, secondaryAnimation, child) {
+                    return SlideTransition(
+                      position: Tween<Offset>(
+                        begin: const Offset(1.0, 0.0),
+                        end: const Offset(0.0, 0.0),
+                      ).animate(animation),
+                      child: child,
+                    );
+                  }));
+            }),
+      ]);
+}
