70e08a33c01ea3614cea26ed418395e99222ae67
Anand Suthar <anandsuthar956@gmail.com>
2024-09-06 19:11:40 +0530

    ✨  feat: Implement PDF generation from form data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
-// import 'package:go_router/go_router.dart';
-
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:open_file/open_file.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -17,80 +17,97 @@ class HomeScreen extends StatelessWidget {
     final TextEditingController dateController = TextEditingController();
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Peedify"),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextFormField(
-                  controller: nameController,
-                  decoration:
-                      const InputDecoration(labelText: 'M/s (Name of Client)'),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter the client name';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: addressController,
-                  decoration: const InputDecoration(labelText: 'Address'),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter the address';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: billNumberController,
-                  decoration:
-                      const InputDecoration(labelText: 'No : (Bill Number)'),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter the bill number';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: dateController,
-                  decoration: const InputDecoration(labelText: 'Date'),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter the date';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16.0),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      // Process the form data
-                      // For example, you can print the data to the console
-                      print('Name: ${nameController.text}');
-                      print('Address: ${addressController.text}');
-                      print('Bill Number: ${billNumberController.text}');
-                      print('Date: ${dateController.text}');
+      appBar: AppBar(
+        title: const Text("Peedify"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                controller: nameController,
+                decoration: const InputDecoration(labelText: 'M/s (Name of Client)'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter the client name';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: addressController,
+                decoration: const InputDecoration(labelText: 'Address'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter the address';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: billNumberController,
+                decoration: const InputDecoration(labelText: 'No : (Bill Number)'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter the bill number';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: dateController,
+                decoration: const InputDecoration(labelText: 'Date'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter the date';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    // Generate PDF
+                    final pdf = pw.Document();
 
-                      // GoRouter.of(context).go("/bill");
+                    pdf.addPage(
+                      pw.Page(
+                        build: (pw.Context context) => pw.Column(
+                          crossAxisAlignment: pw.CrossAxisAlignment.start,
+                          children: [
+                            pw.Text('Client: ${nameController.text}'),
+                            pw.Text('Address: ${addressController.text}'),
+                            pw.Text('Bill Number: ${billNumberController.text}'),
+                            pw.Text('Date: ${dateController.text}'),
+                          ],
+                        ),
+                      ),
+                    );
 
-                    }
-                  },
-                  child: const Text('Submit'),
-                ),
-              ],
-            ),
+                    // Save the PDF
+                    final output = await getTemporaryDirectory();
+                    final file = File("${output.path}/example.pdf");
+                    await file.writeAsBytes(await pdf.save());
+
+                    // Open the PDF
+                    await OpenFile.open(file.path);
+
+                    // Show a snackbar
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('PDF created successfully!')),
+                    );
+                  }
+                },
+                child: const Text('Generate PDF'),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
-
-
 }
