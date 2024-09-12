c5fea9938ba2a926030061024243392bde72d13c
Anand Suthar <anandsuthar956@gmail.com>
2024-09-12 22:54:43 +0530

    feat(ui): implement AppHeader with theme toggling and custom AppBar

--- lib/widgets/app_header.dart
+++ lib/widgets/app_header.dart
@@ -1,34 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:peedify/providers/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class AppHeader extends StatelessWidget implements PreferredSizeWidget {
-  const AppHeader({
-    Key? key,
-  }) : super(key: key);
+  const AppHeader({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: const Color.fromRGBO(30, 90, 255, 0.5),
-      elevation: 0,
-      leading: const Icon(Icons.settings, color: Colors.white),
-      title: const Text(
-        'Peedify',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      centerTitle: true,
-      actions: const [
-        Padding(
-          padding: EdgeInsets.only(right: 16.0),
-          child: Icon(Icons.star, color: Colors.yellow),
-        ),
-      ],
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return AppBar(
+          backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+          elevation: Theme.of(context).appBarTheme.elevation,
+          leading: Icon(Icons.settings,
+              color: Theme.of(context).appBarTheme.foregroundColor),
+          title: Text(
+            'Peedify',
+            style: Theme.of(context).appBarTheme.titleTextStyle,
+          ),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: Icon(
+                themeProvider.themeMode == ThemeMode.dark
+                    ? Icons.light_mode
+                    : Icons.dark_mode,
+                color: Theme.of(context).appBarTheme.foregroundColor,
+              ),
+              onPressed: () {
+                themeProvider.toggleTheme();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight + 10);
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
