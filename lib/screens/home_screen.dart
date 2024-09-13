2d01786502d809972df6f5975c01f9fa47839878
Anand Suthar <anandsuthar956@gmail.com>
2024-09-13 23:52:39 +0530

    fix: Create template

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -206,7 +206,6 @@ class _ColumnNamesDialogState extends State<_ColumnNamesDialog> {
         ElevatedButton(
           child: Text('Create Template', style: textTheme.labelLarge),
           onPressed: () {
-            print('Columns being saved uin home screen : $columnNames');
             widget.templateNotifier.createTemplateWithColumns(
               widget.name,
               widget.shopName,
