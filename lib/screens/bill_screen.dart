52b8c64526883cc47a63673a2d1173b3702674cd
Anand Suthar <anandsuthar956@gmail.com>
2024-09-13 18:41:22 +0530

    refactor: Update ui screens

--- lib/screens/bill_screen.dart
+++ lib/screens/bill_screen.dart
@@ -1,36 +1,44 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
-
-import '../widgets/build_date_field.dart';
-import '../widgets/build_text_field.dart';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:peedify/data/local/database.dart';
+import 'package:peedify/widgets/build_date_field.dart';
+import 'package:peedify/widgets/build_text_field.dart';
 
-class BillDetailsForm extends StatelessWidget {
-  final List<Map<String, String>> billDetailsList = [];
+class BillScreen extends StatefulWidget {
+  final Template template;
+  final List<TemplateColumn> templateColumns; // Updated to accept columns
 
+  const BillScreen({
+    Key? key,
+    required this.template,
+    required this.templateColumns,
+  }) : super(key: key);
+
+  @override
+  _BillScreenState createState() => _BillScreenState();
+}
+
+class _BillScreenState extends State<BillScreen> {
   final _formKey = GlobalKey<FormState>();
-
-  final String sNo = '';
-  final String description = '';
-  final String measurement = '';
-  final String squareFeet = '';
-  final String qty = '';
-  final String rate = '';
-
   final TextEditingController nameController = TextEditingController();
   final TextEditingController addressController = TextEditingController();
   final TextEditingController billNumberController = TextEditingController();
   final TextEditingController dateController = TextEditingController();
 
-  BillDetailsForm({super.key});
+  @override
+  void dispose() {
+    nameController.dispose();
+    addressController.dispose();
+    billNumberController.dispose();
+    dateController.dispose();
+    super.dispose();
+  }
 
   void _showSnackBar(BuildContext context, String message,
       {Color? backgroundColor}) {
-    if (!context.mounted) return;
-
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Text(message),
@@ -44,34 +52,84 @@ class BillDetailsForm extends StatelessWidget {
     if (_formKey.currentState!.validate()) {
       final pdf = pw.Document();
 
-      // Add header information
+      print('Template Columns: ${widget.templateColumns}');
+
       pdf.addPage(
         pw.Page(
           build: (pw.Context context) => pw.Column(
             crossAxisAlignment: pw.CrossAxisAlignment.start,
             children: [
-              pw.Text('Furniture Mart',
+              // Centered shop name
+              pw.Center(
+                child: pw.Text(
+                  widget.template.shopName,
                   style: pw.TextStyle(
-                      fontSize: 24, fontWeight: pw.FontWeight.bold)),
-              pw.Text('Address: Some Address',
-                  style: const pw.TextStyle(fontSize: 12)),
-              pw.Text('Contact: 13444444',
-                  style: const pw.TextStyle(fontSize: 12)),
-              pw.SizedBox(height: 20),
-              pw.Text('Invoice Details',
-                  style: pw.TextStyle(
-                      fontSize: 20, fontWeight: pw.FontWeight.bold)),
+                    fontSize: 24,
+                    fontWeight: pw.FontWeight.bold,
+                  ),
+                ),
+              ),
               pw.SizedBox(height: 10),
-              pw.TableHelper.fromTextArray(
-                context: context,
-                data: <List<String>>[
-                  <String>['Client Name', 'Address', 'Bill Number', 'Date'],
-                  <String>[
-                    nameController.text,
-                    addressController.text,
-                    billNumberController.text,
-                    dateController.text,
-                  ],
+
+              // Address and Phone Number in one row, aligned on both ends
+              pw.Row(
+                mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                children: [
+                  pw.Text(
+                    'Shop Address: ${widget.template.shopAddress}',
+                    style: const pw.TextStyle(fontSize: 12),
+                  ),
+                  pw.Text(
+                    'Contact: ${widget.template.shopPhone}',
+                    style: const pw.TextStyle(fontSize: 12),
+                  ),
+                ],
+              ),
+              pw.SizedBox(height: 20),
+
+              // "Invoice Details" Header
+              pw.Text(
+                'INVOICE DETAILS',
+                style: pw.TextStyle(
+                  fontSize: 20,
+                  fontWeight: pw.FontWeight.bold,
+                ),
+              ),
+              pw.SizedBox(height: 10),
+
+              // Table with headers from the template columns
+              pw.Table(
+                border: pw.TableBorder.all(),
+                children: [
+                  // Table Header (Column names from the template columns)
+                  pw.TableRow(
+                    children: [
+                      for (var column in widget.templateColumns)
+                        pw.Padding(
+                          padding: const pw.EdgeInsets.all(8),
+                          child: pw.Text(
+                            column.columnName, // Column name
+                            style: pw.TextStyle(
+                              fontSize: 14,
+                              fontWeight: pw.FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+
+                  // Mock Data (5 rows of sample data)
+                  for (int i = 0; i < 5; i++)
+                    pw.TableRow(
+                      children: [
+                        for (var column in widget.templateColumns)
+                          pw.Padding(
+                            padding: const pw.EdgeInsets.all(8),
+                            child: pw.Text(
+                                '${column.columnName} $i'), // Sample data
+                          ),
+                      ],
+                    ),
                 ],
               ),
             ],
@@ -85,9 +143,7 @@ class BillDetailsForm extends StatelessWidget {
 
       await OpenFile.open(file.path);
 
-      if (context.mounted) {
-        _showSnackBar(context, 'PDF created successfully!');
-      }
+      _showSnackBar(context, 'PDF created successfully!');
     }
   }
 
@@ -98,7 +154,7 @@ class BillDetailsForm extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Generate Invoice'),
+        title: Text('Generate Invoice', style: theme.textTheme.titleLarge),
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -108,19 +164,20 @@ class BillDetailsForm extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                const SizedBox(height: 20.0),
                 Text(
                   'Invoice Details',
-                  style: theme.textTheme.headlineMedium?.copyWith(
+                  style: theme.textTheme.headlineSmall?.copyWith(
                     color: colorScheme.primary,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                const SizedBox(height: 20.0),
+                const SizedBox(height: 10.0),
                 Card(
                   elevation: theme.cardTheme.elevation,
                   shape: theme.cardTheme.shape,
                   child: Padding(
-                    padding: const EdgeInsets.all(20.0),
+                    padding: const EdgeInsets.all(16.0),
                     child: Column(
                       children: [
                         buildTextField(
@@ -150,14 +207,10 @@ class BillDetailsForm extends StatelessWidget {
                 const SizedBox(height: 30.0),
                 Center(
                   child: ElevatedButton.icon(
-                    icon: Icon(
-                      Icons.picture_as_pdf,
-                      color: colorScheme.onPrimary,
-                    ),
-                    label: Text(
-                      'Generate PDF',
-                      style: TextStyle(color: colorScheme.onPrimary),
-                    ),
+                    icon: Icon(Icons.picture_as_pdf,
+                        color: colorScheme.onPrimary),
+                    label: Text('Generate PDF',
+                        style: TextStyle(color: colorScheme.onPrimary)),
                     onPressed: () => _generatePDF(context),
                     style: theme.elevatedButtonTheme.style,
                   ),
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:peedify/providers/template_provider.dart';
+import 'package:provider/provider.dart';
 import 'package:peedify/widgets/app_header.dart';
 import 'package:peedify/widgets/available_template.dart';
 import 'package:peedify/widgets/empty_state.dart';
@@ -8,29 +10,213 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const isEmpty = false;
-    return Scaffold(
-      appBar: const AppHeader(),
-      body: const SafeArea(
+    return Consumer<TemplateNotifier>(
+      builder: (context, templateNotifier, child) {
+        final isEmpty = templateNotifier.templates.isEmpty;
+        return Scaffold(
+          appBar: const AppHeader(),
+          body: SafeArea(
+            child: Column(
+              children: [
+                Expanded(
+                  child: isEmpty
+                      ? const EmptyStateWidget()
+                      : const AvailableTemplates(),
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              _showCreateTemplateDialog(context, templateNotifier);
+            },
+            child: const Icon(Icons.add_rounded, size: 30),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        );
+      },
+    );
+  }
+
+  void _showCreateTemplateDialog(
+      BuildContext context, TemplateNotifier templateNotifier) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return _CreateTemplateDialog(templateNotifier: templateNotifier);
+      },
+    );
+  }
+}
+
+class _CreateTemplateDialog extends StatefulWidget {
+  final TemplateNotifier templateNotifier;
+
+  const _CreateTemplateDialog({Key? key, required this.templateNotifier})
+      : super(key: key);
+
+  @override
+  State<_CreateTemplateDialog> createState() => _CreateTemplateDialogState();
+}
+
+class _CreateTemplateDialogState extends State<_CreateTemplateDialog> {
+  String name = '';
+  String shopName = '';
+  String shopAddress = '';
+  String shopPhone = '';
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+    return AlertDialog(
+      title: Text('Create New Template', style: textTheme.headlineSmall),
+      content: SingleChildScrollView(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            Expanded(
-              // ignore: dead_code
-              child: isEmpty ? EmptyStateWidget() : AvailableTemplates(),
+            _buildTextField('Template Name', (value) => name = value),
+            const SizedBox(height: 16),
+            _buildTextField('Shop Name', (value) => shopName = value),
+            const SizedBox(height: 16),
+            _buildTextField('Shop Address', (value) => shopAddress = value),
+            const SizedBox(height: 16),
+            _buildTextField('Shop Phone', (value) => shopPhone = value),
+          ],
+        ),
+      ),
+      actions: [
+        TextButton(
+          child: Text('Cancel', style: textTheme.labelLarge),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        ElevatedButton(
+          child: Text('Next', style: textTheme.labelLarge),
+          onPressed: () {
+            Navigator.of(context).pop();
+            _showColumnNamesDialog(
+                context, name, shopName, shopAddress, shopPhone);
+          },
+        ),
+      ],
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+    );
+  }
+
+  TextField _buildTextField(String labelText, Function(String) onChanged) {
+    return TextField(
+      decoration: InputDecoration(labelText: labelText),
+      onChanged: onChanged,
+    );
+  }
+
+  void _showColumnNamesDialog(BuildContext context, String name,
+      String shopName, String shopAddress, String shopPhone) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return _ColumnNamesDialog(
+          templateNotifier: widget.templateNotifier,
+          name: name,
+          shopName: shopName,
+          shopAddress: shopAddress,
+          shopPhone: shopPhone,
+        );
+      },
+    );
+  }
+}
+
+class _ColumnNamesDialog extends StatefulWidget {
+  final TemplateNotifier templateNotifier;
+  final String name;
+  final String shopName;
+  final String shopAddress;
+  final String shopPhone;
+
+  const _ColumnNamesDialog({
+    Key? key,
+    required this.templateNotifier,
+    required this.name,
+    required this.shopName,
+    required this.shopAddress,
+    required this.shopPhone,
+  }) : super(key: key);
+
+  @override
+  State<_ColumnNamesDialog> createState() => _ColumnNamesDialogState();
+}
+
+class _ColumnNamesDialogState extends State<_ColumnNamesDialog> {
+  final List<String> columnNames = [];
+  final TextEditingController columnController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+    return AlertDialog(
+      title: Text('Add Column Names', style: textTheme.headlineSmall),
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            for (var column in columnNames)
+              ListTile(
+                title: Text(column),
+                trailing: IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () {
+                    setState(() {
+                      columnNames.remove(column);
+                    });
+                  },
+                ),
+              ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: columnController,
+              decoration: const InputDecoration(labelText: 'Column Name'),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                final newColumnName = columnController.text.trim();
+                if (newColumnName.isNotEmpty) {
+                  setState(() {
+                    columnNames.add(newColumnName);
+                  });
+                  columnController.clear();
+                }
+              },
+              child: const Text('Add Column'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print("hey");
-        },
-        child: const Icon(
-          Icons.add_rounded,
-          size: 30,
+      actions: [
+        TextButton(
+          child: Text('Cancel', style: textTheme.labelLarge),
+          onPressed: () => Navigator.of(context).pop(),
         ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        ElevatedButton(
+          child: Text('Create Template', style: textTheme.labelLarge),
+          onPressed: () {
+            widget.templateNotifier.createTemplateWithColumns(
+              widget.name,
+              widget.shopName,
+              widget.shopAddress,
+              widget.shopPhone,
+              columnNames,
+            );
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
     );
   }
 }
