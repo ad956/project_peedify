5946f41efcd9bd56956fa0f46c277f81d63243c9
Anand Suthar <anandsuthar956@gmail.com>
2024-09-13 18:40:44 +0530

    refactor: Update go route for bill

--- lib/routes/router_config.dart
+++ lib/routes/router_config.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:peedify/data/local/database.dart';
+import 'package:peedify/providers/template_provider.dart';
 import 'package:peedify/routes/route_constants.dart';
 import 'package:peedify/screens/bill_screen.dart';
 import 'package:peedify/screens/error_screen.dart';
@@ -17,23 +19,32 @@ class PeedifyNavigator {
           builder: (context, state) => const HomeScreen(),
         ),
         GoRoute(
-            name: RouteNames.billScreen,
-            path: "/bill",
-            pageBuilder: (context, state) {
-              return CustomTransitionPage(
-                  key: state.pageKey,
-                  transitionDuration: const Duration(milliseconds: 1300),
-                  child: BillDetailsForm(),
-                  transitionsBuilder:
-                      ((context, animation, secondaryAnimation, child) {
-                    return SlideTransition(
-                      position: Tween<Offset>(
-                        begin: const Offset(1.0, 0.0),
-                        end: const Offset(0.0, 0.0),
-                      ).animate(animation),
-                      child: child,
-                    );
-                  }));
-            }),
+          name: RouteNames.billScreen,
+          path: "/bill",
+          pageBuilder: (context, state) {
+            final arguments = state.extra as Map<String, dynamic>;
+            final template = arguments['template'] as Template;
+            final columns = arguments['columns'] as List<TemplateColumn>;
+
+            return CustomTransitionPage(
+              key: state.pageKey,
+              transitionDuration: const Duration(milliseconds: 1300),
+              child: BillScreen(
+                template: template,
+                templateColumns: columns,
+              ),
+              transitionsBuilder:
+                  (context, animation, secondaryAnimation, child) {
+                return SlideTransition(
+                  position: Tween<Offset>(
+                    begin: const Offset(1.0, 0.0),
+                    end: const Offset(0.0, 0.0),
+                  ).animate(animation),
+                  child: child,
+                );
+              },
+            );
+          },
+        )
       ]);
 }
